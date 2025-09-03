69c7fd8584684626086b38075465bb6236e8b5c3
Himanshu <h_himanshu2007@yahoo.com>
2025-09-03 09:14:22 +0530

    corrected

--- src/main/java/com/resiliance/service/CatalogServiceImpl.java
+++ src/main/java/com/resiliance/service/CatalogServiceImpl.java
@@ -6,6 +6,7 @@ import jakarta.annotation.PostConstruct;
 import org.resilience.dto.CatalogResponse;
 import org.resilience.dto.DiscountResponse;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
@@ -18,8 +19,9 @@ public class CatalogServiceImpl implements CatalogService{
     @Autowired
     private RestTemplate restTemplate;
 
+
     private static final String BASE_URL="http://localhost:8080/discount";
-    private static final String SERVICE_NAME="catalogService";
+    private static final String SERVICE_NAME="catalogServiceImpl";
 
     private List<CatalogResponse> catalogResponseList;
 
@@ -33,29 +35,16 @@ public class CatalogServiceImpl implements CatalogService{
     }
 
     @Override
+    @CircuitBreaker(name = SERVICE_NAME, fallbackMethod = "fallBackDiscountService")
     public List<CatalogResponse> getCatalog(String category) {
         double amount;
-        if(category!=null && !category.isEmpty()){
-            String finalURL=BASE_URL+"?category="+category;
-            DiscountResponse discountResponse=callDiscountService(finalURL);
-            if(discountResponse!=null){
-               amount = discountResponse.getAmount();
-            } else {
-                amount = 0.0;
-            }
-            return catalogResponseList.stream()
-                    .filter(item->item.getCategory().equalsIgnoreCase(category))
-                    .peek(product-> product.setPrice(product.getPrice()-amount))
-                    .toList();
-
+        if(category != null && !category.isEmpty()){
+            String finalURL = BASE_URL + "?category=" + category;
+            ResponseEntity<?> discountResponse = restTemplate.getForEntity(finalURL, DiscountResponse.class);
         }
         return catalogResponseList;
     }
 
-    @CircuitBreaker(name = "discountService", fallbackMethod = "fallBackDiscountService")
-    private DiscountResponse callDiscountService(String finalURL){
-        return restTemplate.getForEntity(finalURL, DiscountResponse.class).getBody();
-    }
 
     private DiscountResponse fallBackDiscountService(Throwable throwable){
         System.out.println("Exception for fallback");
